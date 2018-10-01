f16036b48a96d94979ea7c6397b18c9a49f0fe9c
Benjamin DESROCHES <benjamin.desroches@gmail.com>
2018-10-01 17:22:08 +0200

    recipe list

--- recepies/src/app/recipies/recipe-list/recipe-list.component.ts
+++ recepies/src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'this is simply a test', 'https://www.cbc.ca/food/content/images/recipes/WinterVegPie.jpg'
+    ), new Recipe('A second test Recipe', 'this is simply a second test', 'https://natashaskitchen.com/wp-content/uploads/2017/08/Avocado-Shrimp-Salsa-Recipe-4.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
