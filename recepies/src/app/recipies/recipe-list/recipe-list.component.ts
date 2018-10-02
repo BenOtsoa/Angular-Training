6c8bb3663d61a5f6c0346389727a356df7b8a2f9
Benjamin DESROCHES <benjamin.desroches@gmail.com>
2018-10-02 10:49:50 +0200

    ingredient

--- recepies/src/app/recipies/recipe-list/recipe-list.component.ts
+++ recepies/src/app/recipies/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'this is simply a test', 'https://www.cbc.ca/food/content/images/recipes/WinterVegPie.jpg'
+      // tslint:disable-next-line:max-line-length
     ), new Recipe('A second test Recipe', 'this is simply a second test', 'https://natashaskitchen.com/wp-content/uploads/2017/08/Avocado-Shrimp-Salsa-Recipe-4.jpg')
   ];
   constructor() { }
